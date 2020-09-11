a4a2f41812912fb43be41042eb24e77bcbc447af
charinass <cha_sor@yahoo.co.uk>
2020-09-11 18:03:13 +0200

    commit

--- homework_wordmanipulation_0709.py
+++ homework_wordmanipulation_0709.py
@@ -5,7 +5,7 @@
 # it should not add additional processes but only as the function requires
 # it is best to separate each function and call it from the main where the entire functions are required
 # 2. edit to create into a class
-# please contribute if you want, I want to make this better
+# please contribute if you want, the goal is to be able to use best principles for flexibility
 
 
 class CounterOfStrings:
@@ -18,20 +18,20 @@ class CounterOfStrings:
         return self.newString
 
     def countDistinctString(self):
-        self.wordCount = {}
+        wordCount = {}
         # calling a function within a function inside a class requires 'self.'
         for word in (self.createArrayOfString()):
-            if (word not in self.wordCount):  # append only if unique
-                self.wordCount[word] = self.newString.count(word)
-        wordCount = dict(sorted(self.wordCount.items()))
+            if (word not in wordCount):  # append only if unique
+                wordCount[word] = self.newString.count(word)
+        wordCount = dict(sorted(wordCount.items()))
         return wordCount
 
     def mostFreqWord(self):
         mostFreqWords = []
         callDict = self.countDistinctString()
-        maxCount = max(callDict, key=callDict.get)
+        maxCount = max(callDict.values())
         for key, value in callDict.items():
-            if (value == callDict[maxCount]):
+            if (value == maxCount):
                 mostFreqWords.append(key)
         return mostFreqWords
 
@@ -40,6 +40,8 @@ newInput = input("Enter a sentence: \n").strip(" ")  # enter a string
 strInput = newInput.lower()  # lowercase all letters
 # I want to be able to call each function even without calling them by order
 displayInput = CounterOfStrings(strInput)
-# print(displayInput.createArrayOfString()) #just a trial (my goal is to be able to use each function without depending on the others)
-# print(displayInput.countDistinctString())
-print(displayInput.mostFreqWord())
+# putting comment on each function for trial & error if it works independently
+# (my goal is to be able to use each function without depending on the others)
+print(displayInput.createArrayOfString())  # display str as list
+# print(displayInput.countDistinctString()) # display dict of words
+print(displayInput.mostFreqWord())  # display most frequent word

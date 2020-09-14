2ea732ca212cfdf6cf6b10129c62c4198dfa134e
charinass <cha_sor@yahoo.co.uk>
2020-09-14 19:14:38 +0200

    commit

--- homework_strings_lists_1009.py
+++ homework_strings_lists_1009.py
@@ -1,16 +1,15 @@
 arr = [0, 0, 1, 1, 1, 2, 3, 4, 4, 6, 6, 6, 8, 4, 4]
 arrTmp = []
-arrLen = len(arr)
 arrResult = []
 
-for i in range(arrLen - 1):
+for i in range(len(arr) - 1):
     arrTmp.append(arr[i])
 
     if not (arr[i] == arr[i+1]):
         arrResult.append(arrTmp)
         arrTmp = []
 
-arrTmp.append(arr[arrLen-1])
+arrTmp.append(arr[len(arr)-1])
 arrResult.append(arrTmp)
 
 print(arrResult)
